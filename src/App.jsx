3207953495103d3c5ab8bc61760f987577171401
VIVEK KUMAR <vivekxsh@vivekxsh.vivekxsh>
2024-10-05 01:55:29 +0530

    add Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
 
       <div className="container mx-auto px-8">
         <Navbar />
+        <Hero />
       </div>
 
 
