4d3233ec1b64d43ce2619a7914c439da43c266f2
VIVEK KUMAR <vivekxsh@vivekxsh.vivekxsh>
2024-10-06 03:35:47 +0530

    add about

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import About from "./components/About"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 
@@ -12,6 +13,7 @@ const App = () => {
       <div className="container mx-auto px-8">
         <Navbar />
         <Hero />
+        <About />
       </div>
 
 
