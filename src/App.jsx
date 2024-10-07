aa19a94d1b4eae37fee48eb7b67e507f99a1657d
VIVEK KUMAR <vivekxsh@vivekxsh.vivekxsh>
2024-10-08 01:21:40 +0530

    add technologies

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import About from "./components/About"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
+import Technologies from "./components/Technologies"
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <Navbar />
         <Hero />
         <About />
+        <Technologies />
       </div>
 
 
