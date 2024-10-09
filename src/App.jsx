89f48ad9101ad8acb2ee38afb24c7bd09940d8e9
VIVEK KUMAR <vivekxsh@vivekxsh.vivekxsh>
2024-10-10 02:54:49 +0530

    add achievements

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import About from "./components/About"
+import Achievement from "./components/Achivement"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 import Responsibility from "./components/Responsibility"
@@ -18,6 +19,7 @@ const App = () => {
         <About />
         <Technologies />
         <Responsibility />
+        <Achievement />
       </div>
 
 
