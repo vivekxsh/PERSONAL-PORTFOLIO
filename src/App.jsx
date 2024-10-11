cc900832e22e97bf7a34f69f2b525fc31d96061b
VIVEK KUMAR <vivekxsh@vivekxsh.vivekxsh>
2024-10-12 01:40:59 +0530

    add Contact.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import About from "./components/About"
 import Achievement from "./components/Achivement"
+import Contact from "./components/Contact"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 import Projects from "./components/Projects"
@@ -24,6 +25,7 @@ const App = () => {
         <Achievement />
         <Projects />
         <Resume />
+        <Contact />
       </div>
 
 
